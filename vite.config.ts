4a201827e1f9374c94f96947fe6a057cd34a652c
Seungwon Lee <97718938+Heisnotanimposter@users.noreply.github.com>
2026-08-31 21:17:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  optimizeDeps: {
+    exclude: ['maplibre-gl'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
